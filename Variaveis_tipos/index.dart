cf23261d49fb47ef4f146507a12d1eafe1139f35
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-06-04 14:02:43 -0300

    adicionando estrutra e aula (tipos de variáveis)

---
+++ Variaveis_tipos/index.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  // Introdução a Variáveis
+  String variavelNome = "Mateus";
+  print(variavelNome);
+
+  int valorVariavel = 10; // Inteiro
+  print(valorVariavel);
+
+  bool ehVerdadeiro = true; // Verdadeiro ou falso
+  print(ehVerdadeiro);
+
+  List<String> listaDePalavras = ["Mateus", "Larissa"]; // Todos os itens dentro da lista são Strings
+  print(listaDePalavras[1]);
+
+  // Retornando os dois
+  print('${listaDePalavras[0]} - ${listaDePalavras[1]}');
+
+}
