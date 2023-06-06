f8acdbf5c06c607eea1c7b34e137a35f8cc31674
mateus040 <mateusgabrielmoreno264@gmail.com>
2023-06-06 14:01:37 -0300

    estudos sobre callable interface

---
+++ Callable_Interface(CALL)/index.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Melhorar a semântica do código
+
+  BuscarAlunos buscarAlunos = BuscarAlunos();
+  buscarAlunos();
+}
+
+class BuscarAlunos {
+  void call() => print('Mateus, Larrisa, Maria'); // Callable Interface
+}
